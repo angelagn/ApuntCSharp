2ae6f1c4c245a36185b6f431649116f8e6ba2394
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:42:22 +0200

    Add files via upload

---
+++ s013_Goto/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+{
+
+    class Ejercicio
+    {
+        // Se auto-ejecuta
+    public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Texto reconocido");
+            goto salto1;
+            Console.WriteLine("No imprimible");
+
+            salto1:
+                  Console.WriteLine("He dado salto");
+
+
+        }
+    }
+}
