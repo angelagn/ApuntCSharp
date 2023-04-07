792421205ff8784528702626bf12fe4f86cd44fc
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:55:26 +0200

    Add files via upload

---
+++ s030_InvertirLetras/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            /*3. Escriba un programa en C# que solicite al usuario tres letras y los muestre al revés. */
+
+            Console.WriteLine("Ingrese tres letras: ");
+            String letras = Console.ReadLine();
+            string invertida = "";
+            foreach(char letra in letras){
+                invertida = letra + invertida;
+            }
+            System.Console.WriteLine(invertida);
+        } 
+    }
+}
