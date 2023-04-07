6cfe1e122a4b17500691ad80eb5fbad4c1faa879
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:48:43 +0200

    recursividad

---
+++ s021_EjercicioRecursividad/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            System.Console.WriteLine(recursividad(5));
+            int recursividad(int num){
+                if (num == 0)  return 1;
+                return num* (recursividad(num-1));
+            }
+        } 
+    }
+}
