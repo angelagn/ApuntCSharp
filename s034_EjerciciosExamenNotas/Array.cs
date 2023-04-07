0e8a64326d6a2e0c536f6c7b8380ee2fe1116e0f
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:59:04 +0200

    Add files via upload

---
+++ s034_EjerciciosExamenNotas/Array.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            
+            int mediaNotas(params int[] notas){
+                int suma = 0; 
+                foreach(int i in notas){
+                    suma = suma + i;
+                }
+                int result = suma / 4; 
+                return result;
+            }
+            Console.WriteLine("Resultado: " + mediaNotas(5,7,8, 10));
+        } 
+    }
+}
