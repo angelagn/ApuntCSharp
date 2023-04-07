dfac0103ae327bbaab470e0f71bcf95a2cabca7f
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:40:41 +0200

    new file

---
+++ s011_DoWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+{
+
+    class Ejercicio
+    {
+        // Se auto-ejecuta
+    public static void Main(string[] args)
+        {
+            int num = 0;
+            do{
+                Console.Write(num);
+                num = num + 1;
+            }while(num < 5);
+        }
+    }
+}
