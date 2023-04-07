fa39fa09fe5dcafd736fb588a6e067a30026ebcc
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:57:28 +0200

    Add files via upload

---
+++ s032_EjerciciosExamen/Meeting.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            Meeting("Juan");
+            System.Console.WriteLine(Meeting("Juan"));
+         
+
+            String Meeting(string TempName){
+                return "Encantado de Conocerte " + TempName;
+            }
+        } 
+    }
+}
