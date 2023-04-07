49581d357385e377da8f2b818f5ccef40012ce2c
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:44:35 +0200

    Add files via upload

---
+++ s017_Funciones/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            // // Función que devuelve valor, tiene la pralabra return
+            int Dardoble(int Temp){
+                return(Temp*2);
+            }
+            Dardoble(2);
+
+            Hello obj1 = new Hello(); //Instancia una clase "copia clase"
+            obj1.DardobleMETODO(5);
+            Console.WriteLine(obj1.DardobleMETODO(2));
+            
+        } 
+        public int DardobleMETODO(int temp2){ // Método, está fuera del main
+            return(temp2 * 3);
+        }
+    }
+}
