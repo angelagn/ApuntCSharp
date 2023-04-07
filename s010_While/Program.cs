ddbf6c059bb3b5c383ff853ba1b242e909cb2f4f
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:39:57 +0200

    Add while file

---
+++ s010_While/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace Program
+{
+
+    class Ejercicio
+    {
+        // Se auto-ejecuta
+    public static void Main(string[] args)
+        {
+            string pass = "";
+            while(pass != "Password"){
+            Console.WriteLine("Introducir Pwd: ");
+            pass = Convert.ToString(Console.ReadLine());
+            if(pass == "Password"){
+                Console.WriteLine("Ok, Bienvenido");
+            }else{
+                Console.WriteLine("No entra");
+            }
+            }
+            
+        }
+    }
+}
