961193b4abb6c4d06c1070be5c75ae923216ed97
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:41:21 +0200

    Add files via upload

---
+++ s012_While/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+{
+
+    class While
+    {
+        // Se auto-ejecuta
+    public static void Main(string[] args)
+        {
+            //Hacer un contador
+            int num = 0;
+            while(num <= 5){
+                Console.WriteLine(num);
+                num = num + 1;
+            }
+        }
+    }
+}
