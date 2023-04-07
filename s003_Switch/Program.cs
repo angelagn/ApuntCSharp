7f2bb46c4cb8b1ab09da85585d364eac27170ae2
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:33:16 +0200

    Add switch code

---
+++ s003_Switch/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EjerciciosClase
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           // Switch se usa cuando hay más de 3 preguntas...
+           int day = 2;
+           switch(day){
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2:
+                    Console.WriteLine("Martes");
+                    break;
+                case 3:
+                    Console.WriteLine("Miercoles");
+                    break;
+                case 4:
+                    Console.WriteLine("Jueves");
+                    break;
+                case 5:
+                    Console.WriteLine("Viernes");
+                    break;
+                case 6:
+                    Console.WriteLine("Sábado");
+                    break;
+                case 7:
+                    Console.WriteLine("Domingo");
+                    break;
+            }
+        }
+    }
+}
