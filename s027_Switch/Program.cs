4da91ac70d950fad931a3f73296556147e41ecb6
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:53:11 +0200

    Add files via upload

---
+++ s027_Switch/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+          
+            double num = 2.3;
+            switch(num)
+
+            {
+                case (<= 0.0):
+                System.Console.WriteLine("Loo low");
+                break;
+                case (> 15.0):
+                System.Console.WriteLine("Too Hight");
+                break;
+                default:
+                System.Console.WriteLine("Intermedio: " + num);
+                break;
+            }
+
+
+        } 
+    }
+}
