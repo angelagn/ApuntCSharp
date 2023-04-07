8d66890f8bdf3a817055c627ce3116bbc7086b91
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:43:29 +0200

    Add files via upload

---
+++ s015_Enum/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more informat
+using System;
+
+namespace Program
+{
+
+    class Ejercicio
+
+    
+    {
+    static int varStatic;
+        // Se auto-ejecuta
+    enum EstacionAnio{
+        primavera,
+        verano,
+        otono,
+        invierno
+    }    
+    public static void Main(string[] args)
+        {
+            Console.WriteLine("Valor: "+ EstacionAnio.otono);
+            //Preguntar por los elementos de las enumeraciones
+            Console.WriteLine("Estacion Primavera: "+ EstacionAnio.verano.ToString().Equals("otono"));
+            Console.WriteLine("VAlor numérico: "+ ((int)EstacionAnio.primavera));
+            Console.WriteLine("VAlor numérico: "+ ((int)EstacionAnio.verano));
+            Console.WriteLine("VAlor numérico: "+ ((int)EstacionAnio.otono));
+            Console.WriteLine("VAlor numérico: "+ ((int)EstacionAnio.invierno));
+
+            int indiceValor = (int)EstacionAnio.primavera;
+
+        }
+    }
+}
