c548f7414d2dffdc5b2a36795f5d448e1cace2b9
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:58:07 +0200

    Add files via upload

---
+++ s033_UsuarioLogueado/UsuarioLogueado.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Introducir nombre: ");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Introducir contraseña ");
+            int passw = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introducir frase secreta: ");
+            String fraseSecreta = Console.ReadLine();
+
+            String usuNombre = "Angela";
+            int usuPass = 1234;
+            String usuFrase = "Hola Mundo";
+
+            if (usuNombre == nombre && usuPass == passw && usuFrase == fraseSecreta){
+                System.Console.WriteLine("OK USUARIO LOGUEADO.");
+            }else{
+                System.Console.WriteLine("USUARIO NO LOGUEADO.");
+            }
+
+        } 
+    }
+}
