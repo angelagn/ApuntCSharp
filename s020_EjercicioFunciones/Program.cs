04b372359d8e9b8db5e8d4572f449063cfdc200c
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:47:31 +0200

    Ejercicio Funciones

---
+++ s020_EjercicioFunciones/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+// /*Procedimiento ejercicio
+// -personalizar terminal
+// -limpiar texto terminalñ
+// -logica (programa)
+// -introducir num entero pos
+// solucuion (procedimiento)
+// -par impar*/ 
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+             //Personalizar terminal es darle formato
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            System.Console.WriteLine("Bienvenido");
+
+            int num;
+            System.Console.WriteLine("introducir numero");
+            num = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("\n\n\n" );
+            System.Console.WriteLine("Fin del programa");
+            Console.ReadKey();
+
+            // void parImpar(int tempnum1){
+            //     int x;
+            //     x= (tempnum1 % 2);
+            //     if( x == 0){
+            //         System.Console.WriteLine("Valor par");
+            //     }else{
+            //         System.Console.WriteLine("Valor impar");
+            //     }
+                
+            // }
+        } 
+    }
+}
+
