27c69fdac2377d52d236efa4c5718b4901c33f9c
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:34:57 +0200

    Add returnfile

---
+++ s005_return/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program{
+
+
+class Ejercicio
+{
+    // Se auto-ejecuta
+    static void Main(string[] args) {
+
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine(suma());
+     Console.WriteLine(resta());
+     Console.WriteLine(saludo());
+   
+   //Funcion que devuelve un entero
+    int suma(){
+        int nnumtemoral = 7;
+        return nnumtemoral + 3;
+    }
+    int resta(){
+        int nnumtemoral = 7;
+        return nnumtemoral -3;
+    }
+    // Función que devuelve un string
+    String saludo(){
+        return "Hola";
+    }
+
+    }
+ }
+}
