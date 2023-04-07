61dafe601030a77069549b21b6606d70be4ca670
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:43:58 +0200

    Add files via upload

---
+++ s016_CodigoAscii/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            System.Console.WriteLine("Codigo ASCII"); 
+
+            for (byte a = 33; a<126; a++){
+                Console.Write((char)a); //Paréntesis de transformación o  parseo
+                Console.WriteLine(" => " + a);
+
+            }
+        } 
+    }
+}
