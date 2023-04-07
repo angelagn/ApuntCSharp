69679cdb8f01defb5ca8177184d13fafe0412dc4
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:42:55 +0200

    Add files via upload

---
+++ s014_ForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Program
+{
+
+    class Ejercicio
+    {
+        // Se auto-ejecuta
+    public static void Main(string[] args)
+        {
+            // objeto var = objeto
+            var Loteria2 = new List<int>{23, 11, 13, 7, 16, 33};// new List<int>{}; crea un nuevo objeto de la clase lista
+
+            foreach(int tempValue in Loteria2){ // vamos a recorrer el objeto con la palabra reservada in
+                Console.WriteLine($"{tempValue}");
+            }
+
+        }
+    }
+}
