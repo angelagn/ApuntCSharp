665b4cc92fc57e97d19962c8e593405b7e72a8c1
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-07 10:51:39 +0200

    Add files via upload

---
+++ s025_AppArray2/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+           int[] array1 = {1, 3, 5, 7, 9}; 
+           System.Console.WriteLine("indice 0: " + array1[0]);
+           array1[0] = 5;
+           System.Console.WriteLine("indice 0: " + array1[0]);
+
+            //Procedimiento por parametro y el valor se manda por referencia
+           Change(ref array1);
+            //PASAR EL ARRAY A FUNCIÓN Y HACER CAMBIOS
+           void Change(ref int[] Temparray1){ //Obtiene el objeto array1
+                Temparray1[0] = 1923;
+                System.Console.WriteLine("Ultimo valor en esa posicion: " + Temparray1[0]);
+           }
+        } 
+    }
+}
