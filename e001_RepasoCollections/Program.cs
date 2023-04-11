1b8d2080cac75375ec082bdd58c675cb969a8df7
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-11 16:51:24 +0200

    Add files via upload

---
+++ e001_RepasoCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+namespace HelloWorld 
+{ // crea enumeracion con dias de la semana
+//imprimir tres valores
+    class Hello 
+    {  enum DiasSemana{ //FUERA DEL MAIN
+        lunes,
+        martes,
+        miercoles,
+        jueves,
+        viernes,
+        sabado,
+        domingo
+    }  
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Valor: " + DiasSemana.lunes);
+            Console.WriteLine("Valor: " + DiasSemana.martes);
+            Console.WriteLine("Valor: " + DiasSemana.viernes);
+            //Imprimir el indice de los valores
+            Console.WriteLine("Valor: " + (int)DiasSemana.lunes);
+            Console.WriteLine("Valor: " + (int)DiasSemana.martes);
+            Console.WriteLine("Valor: " + (int)DiasSemana.viernes);
+
+           
+        } 
+    }
+}
