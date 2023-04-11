d7fc1d6544119aa4d86ad1c0b86e46cfaf36a881
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-11 16:53:13 +0200

    Add files via upload

---
+++ e003_EjercicioEnum/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        enum operaciones{SUMA, RESTA, MULTIPLICACION, DIVISION};
+        enum respuesta {SI, NO};
+        enum diasSemana {LUNES, MARTES, MIERCOLES, JUEVES, VIERNES, SABADO, DOMINGO};
+        enum meses{Enero = 1, Febrero = 2, Marzo = 3};
+        enum boolean {FALSE, TRUE};
+        enum playerState {alive, dead, running, fighting};
+        static void Main(string[] args) 
+        { 
+            System.Console.WriteLine("valor: " + operaciones.SUMA); 
+
+            string Temp = operaciones.SUMA.ToString(); //Convierte el valor a texto
+            System.Console.WriteLine(meses.Enero); //Imprime el elemento
+            System.Console.WriteLine((int)meses.Enero); //Imprime el valor
+        } 
+    }
+}
