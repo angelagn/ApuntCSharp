bad08dd53f46386f9ffe0e736a9ed7490b319882
Integra <90930282+angelagn@users.noreply.github.com>
2023-04-21 18:42:25 +0200

    Apunt

---
+++ e015_ListaAArray/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            // Lista de enteros con 5 valores
+            List<int> list = new List<int>();  
+            //Añadir elementos a la lista
+            list.Add(1);
+            list.Add(2);
+            list.Add(2);
+            list.Add(4);
+            list.Add(5);
+
+            //Borrado de  varios elementos
+            list.RemoveAll(item => item == 2); //Recorre la lista
+
+            //Imprimir la nueva lista 
+            foreach(int num in list){
+                System.Console.WriteLine("Value " + num);
+            }
+
+            //Preguntar si hay un valor dentro de la lista
+            System.Console.WriteLine("Si contiene: " + list.Contains(1)); //Preguntas si la lista tiene el valor 1
+
+            //Analizar el valor
+            // bool temp = list.Contains(1){
+            //     if(temp == true){
+            //         // resto de codigo
+            //     }
+            // }
+
+            //Preguntas
+
+            System.Console.WriteLine("Si contiene: " + list.Contains(1));
+            System.Console.WriteLine("Posicion: " + list.IndexOf(1));
+
+            //Comparar varias listas
+            List<int> list2 = new List<int>();
+
+            list2.Add(1);
+            list2.Add(2);
+
+            bool result = list2.Equals(list);
+            System.Console.WriteLine(result);
+
+            //Encontrar elementos dentro de una lista FIND
+            List<String> listCoches = new List<string>() {"BMw", "Citroen", "Seat"}; //Pasar elementos entre llaves
+
+
+            //Se pueden añadir elementos con add
+            listCoches.Add("Twingo");
+
+            foreach(String num in listCoches){
+                System.Console.WriteLine("Value " + num);
+            }
+
+            System.Console.WriteLine("Coche encontrado: \n");
+            System.Console.WriteLine(listCoches.Find(marca => marca == "Citroen")); // crea vaariable temporal marca
+            // Si no corresponde, no imprime nada
+
+            //Segundo modo de encontrar objetos
+            //EXTRAE ELEMENTOS Y LOS GUARDA EN UNA NUEVA LISTA.
+            List<String> listaEncontrar = listCoches.FindAll(elemento => elemento.Contains("w"));
+           
+             foreach(String num in listaEncontrar){
+                System.Console.WriteLine("Valores encontrados " + num);
+            }
+
+            //Compara las listas dato a dato
+            List<String> primeraLista = new List<string>() {"A","B", "C"};
+            List<String> segundaLista = new List<string>() {"b","B", "C"};
+            bool isEqual = primeraLista.SequenceEqual(segundaLista);
+            System.Console.WriteLine(isEqual);
+
+
+
+        } 
+    }
+}
---
+++ e018_Eje1Cola/program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Ejercicio1:
+
+	- Crea una cola de usuarios "Nombres" para hacer espera para recoger unos premios
+	- Imprime todos los usuarios de la cola
+	- Borra el primer elemento en salir de la cola
+	- Imprime todos los usuarios de nuevo */
+
+using System;
+using System.Collections;
+
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+           // - Crea una cola de usuarios "Nombres" para hacer espera para recoger unos premios
+           Queue cola1 = new Queue(); 
+            
+            cola1.Enqueue("Maria");  
+            cola1.Enqueue("juan");
+            cola1.Enqueue("Pedro");
+            cola1.Enqueue("Enrique");
+
+
+            //- Imprime todos los usuarios de la cola
+            System.Console.WriteLine("Lista 1 *********************");
+            foreach(String nombres in cola1){
+                System.Console.WriteLine("Valor: " + nombres);
+            }
+
+            // - Borra el primer elemento en salir de la cola
+            Console.WriteLine("\nBorrando elemento 1'{0}'", cola1.Dequeue() + "\n");
+
+            //- Imprime todos los usuarios de nuevo 
+            System.Console.WriteLine("Lista 2 *********************");
+            foreach(String nombres in cola1){
+                System.Console.WriteLine("Valor: " + nombres);
+            }
+            
+        } 
+    }
+}
