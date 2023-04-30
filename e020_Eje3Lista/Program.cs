d8da63853a6d9074eaf845e6be513c737cdcff71
Angela <90930282+angelagn@users.noreply.github.com>
2023-04-30 11:57:09 +0200

    Add files via upload

---
+++ e020_Eje3Lista/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        /*Ejercicio3:
+
+	- Crea una lista de string para almacenar los valores de una tabla de 5 elementos ( Modelos de Ordenadores )
+	- Añade los elementos de la tabla dentro de la lista
+	- Recorre imprimiendo los valores de la lista almacenados */
+
+        static void Main(string[] args) 
+        { 
+
+            /*Crea una lista de string para almacenar los valores de una tabla de 5 elementos (Modelos de Ordenadores)*/
+            
+            List<string> list = new List<string>();  
+
+            //Añadir elementos a la lista
+            list.Add("Acer");
+            list.Add("Apple");
+            list.Add("Toshiba");
+            list.Add("Asus");
+            list.Add("Lenovo");
+            
+
+	        // - Recorre imprimiendo los valores de la lista almacenados */
+            foreach(string num in list){
+                System.Console.WriteLine("Marca " + num);
+            }
+        } 
+    }
+}
